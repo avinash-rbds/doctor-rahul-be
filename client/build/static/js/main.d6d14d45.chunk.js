a6319f5580d2cb4e24177bffda152526fc754ce4
Avinash Gardas <avinash.g@westagilelabs.com>
2021-09-27 19:06:23 +0530

    enable client/build

---
+++ client/build/static/js/main.d6d14d45.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);var c=e(1),i=e.n(c),o=e(3),u=e.n(o),r=(e(8),e(9),e(0));var s=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,c=t.getFID,i=t.getFCP,o=t.getLCP,u=t.getTTFB;e(n),c(n),i(n),o(n),u(n)}))};u.a.render(Object(r.jsx)(i.a.StrictMode,{}),document.getElementById("root")),s()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.d6d14d45.chunk.js.map
